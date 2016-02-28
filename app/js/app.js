37494ef9ed77c28eee11c63c343c815b2af6d0ad
prasanna ramanujam <prasannaraajkumar@gmail.com>
2016-02-27 19:35:40 -0800

    notification items added

--- app/js/app.js
+++ app/js/app.js
@@ -1,35 +1,49 @@
+import $ from 'jquery';
 import React from 'react';
-import { render } from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
-
+import {render} from 'react-dom';
+import {Router, Route, browserHistory} from 'react-router';
 import PlacesList from './components/PlacesList.jsx';
 
-render((
-     <Router history = { browserHistory }>
-         <Route path="/" component={PlacesList} />
-    </Router>
+render((<Router history = {browserHistory}>
+     <Route path="/" component={PlacesList}/></Router>
 ), document.getElementById('places'))
 
-import jquery from 'jquery';
+
 setTimeout(function() {
     FB.getLoginStatus(function(response) {
       if (response.status === 'connected') {
         console.log('Logged in.');
         var accessToken = response.authResponse.accessToken;
         //  var authObj = FB.getAuthResponse();
-         var url = 'https://graph.facebook.com/me?access_token=' + accessToken;
-         jquery.ajax({
-              type: 'GET',
-              url: url,
-              success: function(data) {
-                  console.log(data);
-                //  callback(data);
-              }
-           });
-    //  }
+         var url = 'https://graph.facebook.com/me?access_token='
+          + accessToken;
+ $.ajax({
+      type: 'GET',
+      url: url,
+      success: function(data) {
+         console.log(data);
+         //  callback(data);
       }
-      else {
-        FB.login();
-      }
+   });
+//  }
+} else {
+FB.login();
+}});
+},
+200);
+
+$(document).ready(function() {
+    $("#notificationContainer").hide();
+
+    $(document).click(function() {
+            $("#notificationContainer").hide();
     });
-},200);
+    console.log('click')
+    $(".activity div").click(function() {
+        console.log('clokc')
+        $("#notificationContainer").fadeToggle(300);
+        $("#notification_count").fadeOut("slow");
+        return false;
+    });
+
+});

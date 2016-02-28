c81f7aee47ec9dadd328ed58502174a3f877c4c9
prasanna ramanujam <prasannaraajkumar@gmail.com>
2016-02-27 17:34:38 -0800

    like, dislike button onclick event added

--- app/js/components/ImageItem.jsx
+++ app/js/components/ImageItem.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 class ImageItem extends React.Component {
    render() {
        let details = this.props.details;
-       let up = 1;
-       let down = 1;
+       let up = this.props.details.up || 0;
+       let down = this.props.details.down || 0;
+       console.log(up);
+       let clickHandler = this.props.clickHandler;
     //    let up = this.
        let replaceStr = details.ThumbnailUrl.substring(details.ThumbnailUrl.indexOf('.jpg') - 2, details.ThumbnailUrl.indexOf('.jpg'));
        let newImageUrl = details.ThumbnailUrl.replace('_t', '_b');
@@ -14,9 +16,9 @@ class ImageItem extends React.Component {
           <img src={newImageUrl}/>
           <div className="name">{details.Name}</div>
                 <div className="interest">
-                    <img className="up" src="/images/Thumbs-Up.png"></img>
+                    <img data-hotelid={details.HotelID} className="up" onClick={clickHandler} src="/images/Thumbs-Up.png"></img>
                     <span>{up}</span>
-                    <img  className="down" src="/images/Thumbs-Down.png"></img>
+                    <img  data-hotelid={details.HotelID} className="down" onClick={clickHandler}  src="/images/Thumbs-Down.png"></img>
                     <span>{down}</span>
                 </div>
           </li>
--- app/js/components/PlacesList.jsx
+++ app/js/components/PlacesList.jsx
@@ -1,35 +1,60 @@
 import React from 'react';
 import ImageItem from './ImageItem.jsx';
 import getAllHotels from './../store/getPlaces';
-
+import $ from 'jquery';
 class PlacesList extends React.Component {
-    constructor() {
-        super();
-        this.state = {placesDetails: undefined};
-    }
-    componentDidMount() {
-        var that = this;
-        getAllHotels(function(data) {
-            that.setState({placesDetails: data});
-        });
-    }
+   constructor() {
+      super();
+      this.state = {
+         placesDetails: undefined
+      };
+   }
+   componentDidMount() {
+      var that = this;
+      getAllHotels(function(data) {
+         let placesDetails = data.HotelInfoList.HotelInfo.slice(0, 4);
+         for (let i = 0; i < placesDetails.length; i++) {
+            placesDetails[i].up = 0;
+            placesDetails[i].down = 0;
+         }
+         that.setState({placesDetails: placesDetails});
+      });
+   }
    render() {
-       let placesDetails;
-       let list;
-       if(this.state.placesDetails) {
-            placesDetails = this.state.placesDetails.HotelInfoList.HotelInfo.slice(0,4);
-          list = placesDetails.map(function(details) {
-             return (<ImageItem details={details}/>);
-          });
-       }
+      let placesDetails = this.state.placesDetails;
+      let list;
+      let that = this;
+      function clickHandler(e) {
+         let hotelId = $(e.target).data('hotelid');
+         if ($(e.target).hasClass('up')) {
+            for (let i = 0; i < placesDetails.length; i++) {
+               if (Number(placesDetails[i].HotelID) === hotelId) {
+                  placesDetails[i].up++;
+               }
+            }
+         } else {
+            for (let i = 0; i < placesDetails.length; i++) {
+               if (Number(placesDetails[i].HotelID) === hotelId) {
+                  placesDetails[i].down++;
+               }
+            }
+         }
+         that.setState({placesDetails: placesDetails});
+      }
+
+      if (this.state.placesDetails) {
+         list = this.state.placesDetails.map(function(details) {
+            return (<ImageItem details={details} clickHandler={clickHandler}/>);
+         });
+      }
 
       return (
 
-          <div className="imageList">
-              <ul>
-                  {list}
-              </ul>
-          </div>
+         <div className="imageList">
+            <ul>
+               {list}
+            </ul>
+         </div>
       )
    }
 }
